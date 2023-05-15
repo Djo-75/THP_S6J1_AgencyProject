01562d4c15f16c872f5781781b921b6aa0dfd01a
Djo-75 <j.pecquet@live.fr>
2023-05-15 20:08:47 +0200

    dark-mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
@@ -6,20 +6,29 @@ import About from './components/About/About'
 import Works from './components/Works/Works'
 import Home from './components/Home/Home'
 import StudyCase from './components/StudyCase/StudyCase'
+import DarkModeToggle from './components/DarkModeToggle/DarkModeToggle'
 
 function App() {
 
+  const [isDarkMode, setIsDarkMode] = useState(true)
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/works" element={<Works />} />
-          <Route path="/works/:projectsSlug-study-case" element={<StudyCase />} /> //routing dynamique avec le slug
-        </Routes>
-      </BrowserRouter>
+      <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
+        <DarkModeToggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/works" element={<Works />} />
+            <Route path="/works/:projectsSlug-study-case" element={<StudyCase />} /> //routing dynamique avec le slug
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
